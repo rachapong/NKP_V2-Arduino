332516141ecc7d0560fa44146382228cda55c388
PrinceBot-Ratthanin <50706633+PrinceBot-Ratthanin@users.noreply.github.com>
2024-11-07 22:18:43 +0700

    Update 01_Test_All_function.ino

--- example/01_Test_All_function/01_Test_All_function.ino
+++ example/01_Test_All_function/01_Test_All_function.ino
@@ -1,17 +1,7 @@
 #include "NKP_V2.h"
-
-void setup() {
-  NKP_V2();
-  wait();
-  set_IMU();
-}
-
-void setup1(){
-
-}
-
-void loop() {
-  display.clearDisplay();
+void readIMU(){
+  for(int i =0;i<6;i++){
+    display.clearDisplay();
   display.setTextSize(1);      
   display.setCursor(30, 0);        
   display.println(F("Read angle"));
@@ -28,8 +18,50 @@ void loop() {
   display.setCursor(38, 45);             
   display.println(Read_angle(2));
   display.display();
-  delay(100);
+  delay(50);
+  }
 }
-void loop1(){
+void setup() {
+  NKP_V2();
+  set_IMU();
+  wait();
+  
+}
+
+void loop() {
+  motor(1,50);
+  motor(2,50);
+  motor(3,50);
+  motor(4,50);
+  motor(5,50);
+  motor(6,50);
+  readIMU();    // delay(1000);
   motor(1,100);
+  motor(2,100);
+  motor(3,100);
+  motor(4,100);
+  motor(5,100);
+  motor(6,100);
+  readIMU();
+  ao();         //motor_stop();
+  readIMU();    // delay(1000);
+  motor(1,-50);
+  motor(2,-50);
+  motor(3,-50);
+  motor(4,-50);
+  motor(5,-50);
+  motor(6,-50);
+  readIMU();    // delay(1000);
+  motor(1,-100);
+  motor(2,-100);
+  motor(3,-100);
+  motor(4,-100);
+  motor(5,-100);
+  motor(6,-100);
+  readIMU();
+  ao();
+  readIMU();  // delay(1000);
+
+
+
 }
